4efe2208bee7320f2dde4daea6a8af5249e80ecd
thoriitsheoga <163763406+thoriitsheoga@users.noreply.github.com>
2024-10-10 16:33:23 +0200

    added basic searchbar functionality using javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert("hello!");
+function search(event) {
+  event.preentDefault();
+  let searchInput = document.querySelector("#search-bar");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
