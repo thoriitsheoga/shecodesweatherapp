db66e0b70e2f5192088a1d9fa27ff192777881b9
thoriitsheoga <163763406+thoriitsheoga@users.noreply.github.com>
2024-10-11 17:04:28 +0200

    used weather api to update the temperature according to location

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temp-value");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "e4ce9c9t0c3f0014ab46bfba167d7oad";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-bar");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
+
+searchCity("Polokwane");
