7895d45ce122e362b580662c0e5da94bef6c9e21
thoriitsheoga <163763406+thoriitsheoga@users.noreply.github.com>
2024-10-12 15:09:49 +0200

    updated website to give real-time weather data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function refreshWeather(response) {
+  //updating city data
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+  //updating weather description
   let descriptionElement = document.querySelector("#weather-condition");
   descriptionElement.innerHTML = response.data.condition.description;
+  //updating humidity description
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  //updating wind-speed
   let windSpeedElement = document.querySelector("#wind-speed");
   windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+  //updating time
   let timeElement = document.querySelector("#time");
-  timeElement.innerHTML = "Friday 17:38";
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
+  //updating temperature
   let temperatureElement = document.querySelector("#temp-value");
   let temperature = Math.round(response.data.temperature.current);
   temperatureElement.innerHTML = temperature;
