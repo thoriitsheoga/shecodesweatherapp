a8d9b599fe49c9ce5deea4da4d144269ac7e447e
thoriitsheoga <163763406+thoriitsheoga@users.noreply.github.com>
2024-10-24 18:40:46 +0200

    integrated the weather api to give real time forecast data

--- src/index.js
+++ src/index.js
@@ -57,29 +57,39 @@ function search(event) {
   let searchInput = document.querySelector("#search-bar");
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 function getForecast(city) {
   let apiKey = "e4ce9c9t0c3f0014ab46bfba167d7oad";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayforecast);
 }
 function displayforecast(response) {
-  days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   forecastHTML = "";
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<section class="weather-forecast-day">
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<section class="weather-forecast-day">
   <div class="weather-forecast">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">🌥️</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <div>
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+    </div>
     <div class="weather-forecast-temp">
-      <div class="weather-forecast-temp-early">9°</div>
+      <div class="weather-forecast-temp-early"><strong>${Math.round(
+        day.temperature.maximum
+      )}°</strong></div>
       <div class="weather-forecast-temp-late">
-        <strong>15°</strong>
+        ${Math.round(day.temperature.minimum)}°
       </div>
     </div>
   </div>
 </section>`;
+    }
   });
   let forecastElement = document.querySelector("#weather-forecast-day");
   forecastElement.innerHTML = forecastHTML;
